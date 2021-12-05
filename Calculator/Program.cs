d1631c00683ce34f8b6db1448078b0c71db6c8b9
NoOne <tntmics@gmail.com>
2021-12-05 15:49:20 +0700

    ya napisal calculator

---
+++ Calculator/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine(); 
+            string[] subs = s.Split(' ');
+            double a = Convert.ToInt32(subs[0]);
+            double b = Convert.ToInt32(subs[2]);
+            string c = Convert.ToString(subs[1]);
+            double results;
+            switch (subs[1])
+            {
+                case "/":
+                    results = a / b;
+                    Console.WriteLine(results);
+                    break;
+                case "*":
+                    results = a * b;
+                    Console.WriteLine(results);
+                    break;
+                case "+":
+                    results = a + b;
+                    Console.WriteLine(results);
+                    break;
+                case "-":
+                    results = a - b;
+                    Console.WriteLine(results);
+                    break;
+                default:
+                    Console.WriteLine("Ты наркоман что-ли?");
+                    break;
+            }
+            Console.ReadLine();
+        }   
+     }
+}
